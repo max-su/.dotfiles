e6fa605b3e0f6cde0a2095303bb42553eee159c9
Max Su <max@maxsu.dev>
2020-06-04 18:21:28 -0400

    zsh, tmux, termite config

--- setup.sh
+++ setup.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
-
 DOTFILES_DIR="$HOME/.dotfiles"
 echo "[*] Setting up dot files"
 
 I3_CONFIG=".config/i3/config"
-ln -s $DOTFILES_DIR/$I3_CONFIG $HOME/$I3_CONFIG
+ln -s -f $DOTFILES_DIR/$I3_CONFIG $HOME/$I3_CONFIG
 
 VIM_CONFIG=".vimrc"
-ln -s $DOTFILES_DIR/$VIM_CONFIG $HOME/$VIM_CONFIG
+ln -s -f $DOTFILES_DIR/$VIM_CONFIG $HOME/$VIM_CONFIG
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 mkdir -p $HOME/.vim/swapfiles
 
 TERMITE_CONFIG=".config/termite/config"
-ln -s $DOTFILES_DIR/$TERMITE_CONFIG $HOME/$TERMITE_CONFIG
+ln -s -f $DOTFILES_DIR/$TERMITE_CONFIG $HOME/$TERMITE_CONFIG
+
+TMUX_CONFIG=".tmux.conf"
+ln -s -f $DOTFILES_DIR/$TMUX_CONFIG $HOME/$TMUX_CONFIG
+
+ZSH_CONFIG=".zshrc"
+ln -s -f $DOTFILES_DIR/$ZSH_CONFIG $HOME/$ZSH_CONFIG
+P10K_ZSH_CONFIG=".p10k.zsh"
+ln -s -f $DOTFILES_DIR/$P10K_ZSH_CONFIG $HOME/$P10K_ZSH_CONFIG
